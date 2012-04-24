cff8522690f66328ddd6bc59da9832ff7ff50ea0
Jeremy Bruestle <jeremy.bruestle@gmail.com>
2012-04-23 22:59:15 -0500

    Boolean tree walker added

---
+++ abtree/tree_walker.h
@@ -0,0 +1,136 @@
+
+#ifndef __tree_walker_h__
+#define __tree_walker_h__
+
+#include "abtree/btree.h"
+#include <boost/iterator/iterator_facade.hpp>
+
+template<class tree_type, class functor_type>
+class forward_subset_iterator;
+
+template<class tree_type, class functor_type>
+class forward_subset_iterator : public boost::iterator_facade<
+                forward_subset_iterator<tree_type, functor_type>,
+                const typename tree_type::value_type,
+                boost::forward_traversal_tag
+	>
+{
+	friend class boost::iterator_core_access;
+	typedef typename tree_type::node_ptr_type node_ptr_t;
+public:
+	typedef typename tree_type::value_type value_type;
+
+	forward_subset_iterator()
+		: m_height(0)
+		, m_curh(0)
+	{}
+
+	forward_subset_iterator(const functor_type& functor, const node_ptr_t& root, size_t height, bool start)
+		: m_functor(functor)
+		, m_height(height)
+		, m_curh(-1)
+		, m_ptr(height)
+		, m_idx(height)
+		, m_value(NULL)
+	{
+		if (start && height != 0)
+		{
+			//printf("Generating start\n");
+			m_curh = 0;
+			m_ptr[0] = root;
+			m_idx[0] = 0;
+			next_valid();
+		}
+		if (m_curh != -1)
+			m_value = new value_type(m_ptr[m_curh]->key(m_idx[m_curh]), m_ptr[m_curh]->val(m_idx[m_curh]));
+	}
+private:
+
+	functor_type m_functor;
+	int m_height;
+	int m_curh;
+	std::vector<node_ptr_t> m_ptr;
+	std::vector<int> m_idx;
+	value_type* m_value;
+
+	void next_valid()
+	{
+		//printf("next valid\n");
+		assert(m_curh != -1);
+		assert(m_idx[m_curh] < m_ptr[m_curh]->size());
+		while(m_curh != -1)
+		{
+			// If the current element matches
+			//printf("Checking functor on %d\n", m_ptr[m_curh]->val(m_idx[m_curh]));
+			if (m_functor(m_ptr[m_curh]->val(m_idx[m_curh]))) 
+			{
+				//printf("match\n");
+				// If we are at the bottrom
+				if (m_curh == m_height - 1)
+					return;  // Found a valid low level entry
+				// Otherwise, go down a level
+				//printf("Going down a level\n");
+				m_ptr[m_curh + 1] = m_ptr[m_curh]->ptr(m_idx[m_curh]);
+				m_idx[m_curh + 1] = 0;
+				m_curh++;
+			}
+			else
+			{
+				forward_up();
+			}
+		}
+	}
+			
+	void forward_up()
+	{
+		// Go forward one
+		//printf("forward up: m_curh = %d,m_idx[m_curh]=%d\n", m_curh, m_idx[m_curh]);
+		m_idx[m_curh]++;
+		// While we are at the end at the current level
+		while(m_idx[m_curh] == m_ptr[m_curh]->size())
+		{
+			// Go up one
+			//printf("  forward up: m_curh = %d,m_idx[m_curh]=%d\n", m_curh, m_idx[m_curh]);
+			m_ptr[m_curh] = node_ptr_t();
+			m_idx[m_curh] = 0;
+			m_curh--;
+			// If at the top, break
+			if (m_curh == -1) break;
+			// Go forward at this level
+			m_idx[m_curh]++;
+		}
+	}
+
+	void increment()
+	{
+		//printf("Incrementing\n");
+		forward_up();
+		if (m_curh != -1)
+			next_valid();
+		delete m_value;
+		if (m_curh != -1)
+			m_value = new value_type(m_ptr[m_curh]->key(m_idx[m_curh]), m_ptr[m_curh]->val(m_idx[m_curh]));
+		else
+			m_value = NULL;
+	}
+	bool equal(const forward_subset_iterator& rhs) const
+	{
+		return m_curh && rhs.m_curh && m_ptr == rhs.m_ptr &&  m_idx == rhs.m_idx;
+	}
+	const value_type& dereference() const { return *m_value; }
+};
+
+template<class tree_type, class functor_type>
+std::pair<
+	forward_subset_iterator<tree_type, functor_type>, 
+	forward_subset_iterator<tree_type, functor_type>
+>
+forward_subset(const tree_type& tree, const functor_type& functor)
+{
+	return std::make_pair(
+		forward_subset_iterator<tree_type, functor_type>(functor, tree.get_root(), tree.get_height(), true),
+		forward_subset_iterator<tree_type, functor_type>(functor, tree.get_root(), tree.get_height(), false)
+	);
+}
+
+#endif
---
+++ test/walker.cpp
@@ -0,0 +1,51 @@
+
+#include "abtree/abtree.h"
+#include "abtree/tree_walker.h"
+#include "gtest/gtest.h"
+#include <boost/foreach.hpp>
+#define foreach BOOST_FOREACH
+
+class is_greater
+{
+public:
+	is_greater(int min) : m_min(min) {}
+	bool operator()(int x) { return x > m_min; }
+private:
+	int m_min;
+};
+
+class max_int
+{
+public:
+	void operator()(int& a, const int& b) { a = std::max(a,b); }
+};
+
+TEST(walker, basic)
+{
+	typedef abtree<int, int, max_int> tree_type;
+	tree_type tree;
+	typedef std::map<int, int> map_type;
+	map_type map;
+	printf("Doing inserts\n");
+	for(size_t i = 0; i < 1000; i++)
+	{
+		int k = random()%1000;
+		int v = random()%1000;
+		tree.insert(std::make_pair(k,v));
+		map.insert(std::make_pair(k,v));
+	}
+	printf("Doing walk\n");
+	is_greater f(700);
+	map_type::iterator it = map.begin();
+	while(it != map.end() && !f(it->second)) ++it;
+	foreach(const tree_type::value_type& kvp, forward_subset(tree, f))
+	{
+		printf("(%d, %d) vs (%d, %d)\n", kvp.first, kvp.second, it->first, it->second);	
+		assert(it != map.end());
+		assert(kvp.first == it->first);
+		assert(kvp.second == it->second);
+		++it;
+		while(it != map.end() && !f(it->second)) ++it;
+	}
+	printf("Done\n");
+}
